f6c9a09d506dbc06e273a2c7a5e71ff9c8539ef5
Adrian <adicirstei@gmail.com>
2015-01-06 17:41:18 +0200

    ignored own tweets

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var Twit = require('twit');
 var cfg = require('./config.json');
 
-var t = new Twit(cfg);
+var t = new Twit(cfg.twitter);
 
-var stream = t.stream('statuses/filter', { track: 'webrtc' });
+var stream = t.stream('statuses/filter', { track: 'webrtc,nodejs,easyrtc' });
 
 stream.on('tweet', function (tweet) {
-  console.log(tweet)
-})
+  console.log( JSON.stringify( tweet.entities.hashtags));
+  if (tweet.user.id_str === cfg.my_id) return;
+  t.post('statuses/update', { status: tweet.text }, function(err, data, response) {
+    if (err) console.log(err);
+  });
+});
 
