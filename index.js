78af4df6a77854b8219648e9a87669402406e018
Adrian <adicirstei@gmail.com>
2015-01-06 16:20:19 +0200

    ceva

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-var T = require('twit');
+var Twit = require('twit');
 var cfg = require('./config.json');
+
+var t = new Twit(cfg);
+
+var stream = t.stream('statuses/filter', { track: 'webrtc' });
+
+stream.on('tweet', function (tweet) {
+  console.log(tweet)
+})
 
