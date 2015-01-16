179e703802c481de8ff770335e9d46ed4db36d81
Adrian <adicirstei@gmail.com>
2015-01-16 15:55:47 +0200

    editor config

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*jslint node: true*/
+
+'use strict';
+
 var Twit = require('twit');
 var cfg = require('./config.json');
 
@@ -11,23 +15,34 @@ var MINUTE = 60 * 1000;
 
 stream.on('tweet', function (tweet) {
   
-  var now = new Date();
-  var rom = JSON.stringify(tweet.place).match(/Romania/);
-  
-  
-  if (tweet.lang !== 'en' && tweet.lang !== 'ro') return;
-  if (tweet.user.id_str === cfg.my_id) return;
-  if (tweet.text.match(/'^RT /)) return;
-  if (tweet.entities.user_mentions.length > 0) return;
-  
-  if ( !(rom || Math.random() < 0.25) ) return;
-  if ( !rom && (now.getTime() - lastRT.getTime()) < 5 * MINUTE ) return;
-  
-  console.log( tweet );
-  
-  lastRT = now;
-  t.post('statuses/retweet/:id', { id: tweet.id_str }, function(err, data, response) {
-    if (err) console.log(err);
-  });
+    var now = new Date(), rom = JSON.stringify(tweet.place).match(/Romania/);
+
+    if (tweet.lang !== 'en' && tweet.lang !== 'ro') {
+        return;
+    }
+    if (tweet.user.id_str === cfg.my_id) {
+        return;
+    }
+    if (tweet.text.match(/'^RT /)) {
+        return;
+    }
+    if (tweet.entities.user_mentions.length > 0) {
+        return;
+    }
+
+    if (!(rom || Math.random() < 0.25)) {
+        return;
+    }
+    if (!rom && (now.getTime() - lastRT.getTime()) < 5 * MINUTE) {
+        return;
+    }
+    console.log(tweet);
+
+    lastRT = now;
+    t.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
+        if (err) {
+            console.log(err);
+        }
+    });
 });
 
